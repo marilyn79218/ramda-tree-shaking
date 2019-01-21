c825dd0d7a0b6b2de14d4b779b6c26b8a3c6356e
Ray Lei <raylei@ISTWLAPAPPLE1811.local>
2019-01-21 17:18:03 +0800

    06 - 1. Add webpack-bundle-analyzer. 2. Implement tree shaking in multi-entries.

---
+++ 06-webpack-scope-hoisted/src/script.js
@@ -0,0 +1,3 @@
+import * as R from "ramda"
+
+console.log("Script - R.identity(1): ", R.identity(1))
--- 06-webpack-scope-hoisted/webpack.config.js
+++ 06-webpack-scope-hoisted/webpack.config.js
@@ -1,6 +1,7 @@
 let path = require("path")
 let UglifyJSPlugin = require("uglifyjs-webpack-plugin")
 let webpack = require("webpack")
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 let env = process.env.NODE_ENV
 let minifyJS = env == "production"
@@ -10,18 +11,38 @@ let basePath = __dirname
 module.exports = {
   entry: {
     "index": "./src/index.js",
+    // Webpack tree shaking is broke when mutiple entries
+    // ref: https://github.com/webpack/webpack/issues/4453
+    // "script": "./src/script-2.js"
   },
   output: {
     path: path.join(basePath, "dist"),
     filename: "[name].js",
   },
   plugins: [
-    new webpack.DefinePlugin({
-     "process.env": {
-        NODE_ENV: JSON.stringify(process.env.NODE_ENV)
-      }
-    }),
+    new BundleAnalyzerPlugin(),
     new webpack.optimize.ModuleConcatenationPlugin(),
     ...(minifyJS ? [new UglifyJSPlugin()] : [])
   ],
 }
+
+// // Solution to achieve tree shaking in mutiple entries
+// // ref: https://github.com/FormidableLabs/webpack-tree-shaking-multiple-entry-points/blob/master/webpack.config.array.js
+// const configGenerator = (entryName, index) => ({
+//   entry: {
+//     [entryName]: "./src/" + entryName + ".js"
+//   },
+//   output: {
+//     path: path.join(basePath, "dist"),
+//     filename: "[name].js",
+//   },
+//   plugins: [
+//     new BundleAnalyzerPlugin({
+//       analyzerPort: 8888 + index,
+//     }),
+//     new webpack.optimize.ModuleConcatenationPlugin(),
+//     ...(minifyJS ? [new UglifyJSPlugin()] : [])
+//   ],
+// });
+
+// module.exports = entryNames.map((entryName, index) => configGenerator(entryName, index));
